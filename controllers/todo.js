2da2a5f73511302bfed85e902e816faf6143272d
Timothy-py <adeyeyetimothy33@gmail.com>
2020-12-17 04:42:32 +0100

    added validations to todo create and update inputs

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,81 +1,139 @@
-const { Result } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 const Todo = require('../models/todo')
 
 
-// Create a Todo
-exports.createNewTodo = (req, res) => {
-    
-    // receive data from the request body and save individually into a variable
-    const todo_name = req.body.todo_name;
-    const status = req.body.status;
-    const start_time = Date.parse(req.body.start_time);
-    const end_time = Date.parse(req.body.end_time);
-    const description = req.body.description;
-    const priority = req.body.priority;
+// function to hold a list of all the validations required for creating and updating a todo item
+function validators(){
+    return [
+        // validate inputs from req.body
+        body('todo_name')
+        .isLength({min:3, max:30}).withMessage("todo_name must be between 3 and 30 characters long")
+        .notEmpty().withMessage('todo_name cannot be empty'),
 
-    // Create a new todo item
-    const newTodo = new Todo({
-        todo_name,
-        status,
-        start_time,
-        end_time,
-        description,
-        priority
-    })
+        body('status')
+        .isBoolean().withMessage("status can either be true or false"),
 
-    // Insert the todo item into the DB
-    newTodo.save()
-    .then((todoItem) => {
-        res.status(200).json({
-            message: "ToDo Item Created Successfully",
-            data: todoItem
-        })
-    })
-    .catch((error) => {
-        res.status(400).json({
-            message: `Unable to create Todo Item: ${error}`,
-        })
-    })
+        body('start_time')
+        .notEmpty().withMessage('start_time cannot be empty'),
 
+        body('end_time')
+        .notEmpty().withMessage('end_time cannot be empty'),
+
+        body('description')
+        .isString().withMessage("description must be string/text characters")
+        .notEmpty().withMessage('description cannot be empty'),
+
+        body('priority')
+        .isIn(["high", "low", "medium"]).withMessage("Invalid priority! Priority should be set to high, low or medium")
+        .notEmpty().withMessage('Priority cannot be empty')
+    ]
 }
 
 
-// Update a Todo Item
-exports.updateTodo = (req, res) => {
+// Create a Todo
+exports.createNewTodo = [
 
-    // find the todo item through the id passed in the url
-    Todo.findById(req.params.id)
-    .then((todoItem) => {
-        todoItem.todo_name = req.body.todo_name;
-        todoItem.status = req.body.status;
-        todoItem.start_time = req.body.start_time;
-        todoItem.end_time = req.body.end_time;
-        todoItem.description = req.body.description,
-        todoItem.priority = req.body.priority
+    // execute validator function
+    validators(),
 
-        // now save the new todo item: update
-        todoItem.save()
-        .then((newTodo) => {
+    async (req, res) => {
+
+        // check for validations
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({
+                errors: errors.array()
+            })
+        }
+
+        // receive data from the request body and save individually into a variable
+        const todo_name = req.body.todo_name;
+        const status = req.body.status;
+        const start_time = Date.parse(req.body.start_time);
+        const end_time = Date.parse(req.body.end_time);
+        const description = req.body.description;
+        const priority = req.body.priority;
+
+        // Create a new todo item
+        const newTodo = new Todo({
+            todo_name,
+            status,
+            start_time,
+            end_time,
+            description,
+            priority
+        })
+
+        // Insert the todo item into the DB
+        await newTodo.save()
+        .then((todoItem) => {
             res.status(200).json({
-                message: "ToDo Item Updated Succesfully",
-                data: newTodo
+                message: "ToDo Item Created Successfully",
+                data: todoItem
             })
         })
         .catch((error) => {
             res.status(400).json({
-                message: `Unable to Update the ToDo Item ${error}`
+                message: `Unable to create Todo Item: ${error}`,
             })
         })
 
-    })
-    .catch((error) => {
-        res.status(400).json({
-            message: `Cannot find the ToDo with the id:(${req.params.id}): Error:${error}`,
-            status: false
-        })
-    })
+    }
 
-};
+
+]
+
+
+// Update a Todo Item
+exports.updateTodo = [
+
+    // execute validator function
+    validators(),
+
+    (req, res) => {
+
+        // check for validations
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({
+                errors: errors.array()
+            })
+        }
+
+        // find the todo item through the id passed in the url
+        Todo.findById(req.params.id)
+        .then((todoItem) => {
+            todoItem.todo_name = req.body.todo_name;
+            todoItem.status = req.body.status;
+            todoItem.start_time = req.body.start_time;
+            todoItem.end_time = req.body.end_time;
+            todoItem.description = req.body.description,
+            todoItem.priority = req.body.priority
+
+            // now save the new todo item: update
+            todoItem.save()
+            .then((newTodo) => {
+                res.status(200).json({
+                    message: "ToDo Item Updated Succesfully",
+                    data: newTodo
+                })
+            })
+            .catch((error) => {
+                res.status(400).json({
+                    message: `Unable to Update the ToDo Item ${error}`
+                })
+            })
+
+        })
+        .catch((error) => {
+            res.status(400).json({
+                message: `Cannot find the ToDo with the id:(${req.params.id}): Error:${error}`,
+                status: false
+            })
+        })
+
+        }
+];
 
 // Delete a Todo Item
 exports.deleteTodo = (req, res) => {
